604199304c5a2f5132f4d8ad17991eb028fe7dac
Starishine <sania.amarnath@gmail.com>
2026-01-07 16:50:34 +0800

    Modify allow headers

--- internal/router/router.go
+++ internal/router/router.go
@@ -18,7 +18,7 @@ func SetupRouter() *gin.Engine {
 	r.Use(cors.New(cors.Config{
 		AllowOrigins:     []string{frontendURL},
 		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
-		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization", "Accept"},
 		ExposeHeaders:    []string{"Content-Length"},
 		AllowCredentials: true,
 		MaxAge:           12 * time.Hour,
